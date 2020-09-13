719497967c38adb1d7afadfe599c52c474bbe5d5
marigotten <[hum_or_@outlook.com]>
2020-09-14 08:36:18 +0900

    add first geometry

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
 
-function App() {
+const Thing = () => {
+  const ref = useRef();
+  useFrame (() => (ref.current.rotation.z += 0.01));
+  return(
+    <mesh
+    ref = {ref}
+    onClick={e => console.log('click')}
+    onPointerOver={e => console.log('hover')}
+    onPointerOut={e => console.log('unhover')}
+    >
+    <planeBufferGeometry attach='geometry' args={[1,1]} />
+    <meshBasicMaterial
+    attach='material'
+    color='hotpink'
+    opacity='0.5'
+    />
+    </mesh>
+  )
+}
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Canvas>
+      <Thing />
+    </Canvas>
+    );
 }
 
 export default App;
