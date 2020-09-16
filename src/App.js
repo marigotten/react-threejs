fd7693b136f2aeb5fa4d947351a6031d69788db2
marigotten <[hum_or_@outlook.com]>
2020-09-16 22:22:43 +0900

    add more boxes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import React, {useRef} from 'react';
-import { Canvas, useFrame } from 'react-three-fiber';
+/** @jsx jsx */
+import { css, jsx, Global } from '@emotion/core';
+import emotionReset from 'emotion-reset';
+import { Work } from './work';
 
-const Thing = () => {
-  const ref = useRef();
-  useFrame (() => (ref.current.rotation.z += 0.01));
-  return(
-    <mesh
-    ref = {ref}
-    onClick={e => console.log('click')}
-    onPointerOver={e => console.log('hover')}
-    onPointerOut={e => console.log('unhover')}
-    >
-    <planeBufferGeometry attach='geometry' args={[1,1]} />
-    <meshBasicMaterial
-    attach='material'
-    color='hotpink'
-    opacity='0.5'
-    />
-    </mesh>
-  )
+const globalStyles = css`
+${emotionReset}
+*, *::after, *::before {
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  font-smoothing: antialiased;
 }
+`;
+
 
 const App = () => {
-  return (
-    <Canvas>
-      <Thing />
-    </Canvas>
+    return (
+      <div>
+              <Global styles={globalStyles} />
+              <Work />
+        </div>
     );
-}
+};
 
 export default App;
---
+++ src/work.js
@@ -0,0 +1,70 @@
+/** @jsx jsx */
+import * as THREE from 'three';
+import * as CANNON from 'cannon';
+import React, { useEffect,useState, useContext } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { css, jsx } from '@emotion/core'
+import { useCannon, Provider } from './useCannon';
+// import { PlaneBufferGeometry } from 'three';
+
+const theme = css`
+    width: 100%;
+    height: 100vh;
+    background-color: #272727;
+`;
+
+const Plane = ({position}) => {
+    const ref = useCannon({ mass: 0 }, body => {
+        body.addShape(new CANNON.Plane());
+        body.position.set(...position);
+    });
+    return (
+        <mesh ref={ref} receiveShadow>
+            <planeBufferGeometry attach='geometry' args={[1000,1000]} />
+            <meshPhongMaterial attach='material' color='#272727' />
+        </mesh>
+    );
+};
+
+const Box = ({ position, args }) => {
+    const ref = useCannon({ mass: 100000 }, body => {
+        body.addShape(new CANNON.Box(new CANNON.Vec3(1,1,1,)));
+        body.position.set(...position);
+    });
+    return (
+        <mesh ref={ref} castShadow receiveShadow>
+            <boxGeometry attach='geometry' args={[2,2,2]} />
+            <meshStandardMaterial attach='material' />
+        </mesh>
+    )
+};
+
+export const Work = () => {
+    return(
+        <div css={theme}>
+            <Canvas camera={{position: [0,5,30]}}>
+            {/* ambientLight = 散乱する光源によって、オブジェクトに対し均等に光を当てる。影(cast shadow)は出来ない。 */}
+            {/* intensity = 光の強さ */}
+            {/* penumbra = ターゲット(向けている方向の中心)からの距離による光の減衰値。デフォルトは0 */}
+            <ambientLight intensity={0.2} />
+            <spotLight
+            intensity={0.6}
+            position={[30,30,50]}
+            angle={0.2}
+            penumbra={1}
+            castShadow
+            />
+            <Provider>
+                <Plane position={[0,0,-10]} />
+                    <Box position={[1, 0, 1]} args={[2, 2, 2]} />
+                    <Box position={[1, 0, 1]} args={[1, 1, 5]} />
+                    <Box position={[2, 1, 5]} args={[3, 3, 3]} />
+                    <Box position={[0, 0, 6]} args={[2, 4, 2]} />
+                    <Box position={[-1, 1, 8]} args={[2, 3, 2]} />
+                    <Box position={[0, 2, 3]} args={[5, 5, 1]} />
+                    <Box position={[2, -1, 13]} args={[1, 1, 10]} />
+            </Provider>
+            </Canvas>
+        </div>
+    );
+};
