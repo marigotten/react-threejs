add9e5668aedd9ae08c255d84ff8f9d4b63557e1
marigotten <[hum_or_@outlook.com]>
2021-03-14 18:53:05 +0900

    add elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 /** @jsx jsx */
-import { css, jsx, Global } from '@emotion/core';
+import { css, jsx, Global} from '@emotion/core';
 import emotionReset from 'emotion-reset';
-import { Work } from './work';
+import { VFXProvider } from 'react-vfx';
+import * as VFX from 'react-vfx';
+// import { Work } from './work';
+// import { Vfx } from './vfx';
 
 const globalStyles = css`
 ${emotionReset}
 *, *::after, *::before {
-  box-sizing: border-box;
-  -webkit-font-smoothing: antialiased;
-  font-smoothing: antialiased;
+box-sizing: border-box;
+-webkit-font-smoothing: antialiased;
+font-smoothing: antialiased;
+}
+body {
+  background-color: #333;
+  text-align: center;
+}
+h1 {
+  display: block;
+  font-size: 3.2rem;
+  color: #fff;
+  font-weight: 900;
 }
 `;
 
 
 const App = () => {
-    return (
-      <div>
-              <Global styles={globalStyles} />
-              <Work />
-        </div>
-    );
+return (
+<div>
+  <Global styles={globalStyles} />
+  <VFX.VFXProvider>
+
+  <h1>React VFXで<br />遊んでみた</h1>
+  <VFX.VFXImg src="logo512.png" alt="react logo" shader="sinewave"/>
+  <VFX.VFXSpan shader="rgbshift">Hi there!</VFX.VFXSpan>
+  <VFX.VFXImg
+      alt="tom and jerry"
+      src="tom-jerry-1.gif"
+      shader="halftone"/>
+  <VFX.VFXImg
+      alt="tom and jerry"
+      src="otamu.jpg"
+      shader="glitch"/>
+  </VFX.VFXProvider>
+</div>
+);
 };
 
 export default App;
---
+++ src/vfx.js
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+import * as VFX from 'react-vfx';
+
+export const Vfx = () => {
+return (
+{/* Render text as image, then apply the shader effect! */}
+<a href="https://example.com">
+  <VFXSpan>Yo</VFXSpan>
+</a>
+
+);
+};
